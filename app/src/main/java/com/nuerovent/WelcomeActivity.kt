e18964a1d1990730057cad4f0c91e4e4f74f2f46
koturaderrick <koturaderrickamu@gmail.com>
2025-06-27 12:45:39 +0300

    Initial commit

---
+++ app/src/main/java/com/nuerovent/WelcomeActivity.kt
@@ -0,0 +1,19 @@
+package com.nuerovent
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
